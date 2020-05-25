bd7be5efe451100aeb6f71ba81f1676d670f7168
William Hermozo <william.hermozo@protonmail.com>
2020-05-25 16:54:47 +0200

    add title to home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      
+      <h1 className='home__title'>This is a 1-hour react coding challenge</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
